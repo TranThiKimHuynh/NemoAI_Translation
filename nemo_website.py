64cf74ce68f295afbae4b28237ac5085909851ad
TranThiKimHuynh <kimhuynhstudy12@gmail.com>
2024-06-15 16:46:10 +0700

    add model , change voice input option : only use .WAV

--- nemo_website.py
+++ nemo_website.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import time
 from PIL import Image
+from lib import convert_image_to_text, speech_to_text
+from transformers import MarianMTModel, MarianTokenizer
 
-from googletrans import Translator
-from streamlit_webrtc import webrtc_streamer,WebRtcMode
-from lib import convert_image_to_text, AudioProcessor
-
-
-translator = Translator()
+model_path = 'fine-tuned-mt-en-vi'
+tokenizer = MarianTokenizer.from_pretrained(model_path)
+model = MarianMTModel.from_pretrained(model_path)
 
 page_bg = """
 <style>
@@ -55,17 +54,6 @@ text-align: center;
     display: inline-block;
     margin-bottom: 10px;
 }
-.icon-container {
-    display: flex;
-    justify-content: center;
-    gap: 20px;
-    margin-bottom: 20px;
-}
-.icon-container img {
-    width: 50px;
-    height: 50px;
-    cursor: pointer;
-}
 .arrow-container {
     display: flex;
     justify-content: center;
@@ -87,25 +75,17 @@ st.markdown('''
     <h1 class="custom-title">NEMO AI TRANSLATION  [- -] </h1>
 </div>
 ''', unsafe_allow_html=True)
-st.image("Bot.png", width=150)
+st.image("ui-res/Bot.png", width=150)
 
 # Radio button to choose input type
-input_type = st.radio("Choose input type:", ("Text", "Voice", "Image"), index=0)
+input_type = st.radio("Choose input type:", ("Text", "Image", "Voice"), index=0)
+
+input_text = ""
 
 # Input text areas
 if input_type == "Text":
     st.markdown('<div class="custom-label">ENGLISH</div>', unsafe_allow_html=True)
     input_text = st.text_area("", height=150, key="input_text", placeholder="Enter text here...", label_visibility="collapsed")
-
-elif input_type == "Voice":
-    st.markdown('<div class="custom-label">VOICE INPUT</div>', unsafe_allow_html=True)
-    audio_processor = AudioProcessor()
-    webrtc_ctx = webrtc_streamer(key="key", mode=WebRtcMode.SENDONLY, audio_processor_factory=lambda: audio_processor, media_stream_constraints={"audio": True})
-
-    if webrtc_ctx.state.playing:
-        input_text = audio_processor.recognize_speech()
-        st.markdown("### Extracted Text")
-        st.text_area("", input_text, height=150, key="output_text", label_visibility="collapsed")
 
 elif input_type == "Image":
     st.markdown('<div class="custom-label">IMAGE INPUT</div>', unsafe_allow_html=True)
@@ -116,11 +96,20 @@ elif input_type == "Image":
         
         with st.spinner('Processing image...'):
             input_text = convert_image_to_text(image)
-      
+elif input_type == "Voice":
+    st.write("Upload an audio file and convert it to text.")
+
+    uploaded_file = st.file_uploader("Choose an audio file", type=["wav"], key="input_audio")
+
+    if uploaded_file is not None:
+        file_details = {"Filename": uploaded_file.name, "FileType": uploaded_file.type}
+        st.write(file_details)
+
+        with st.spinner('Processing audio...'):
+            input_text = speech_to_text(uploaded_file)
 
 # Further processing of input_text if needed
-if input_type == "Text" or (input_type == "Image" and uploaded_image is not None):
-    # Process the input_text as needed
+if input_text:
     st.write("Processed Text:", input_text)
 
 m = st.markdown("""
@@ -131,27 +120,26 @@ div.stButton > button:first-child {
 </style>""", unsafe_allow_html=True)
 
 ## Translate function
-def translate_text(text, src="en", dest="vi"):
-    translation = translator.translate(text, src=src, dest=dest)
-    return translation.text
+def translate_text(text):
+    inputs = tokenizer.encode(text, return_tensors="pt", truncation=True, padding="max_length", max_length=64)
+    translated_tokens = model.generate(inputs, max_length=64, num_beams=4, early_stopping=True)
+    translated_text = tokenizer.decode(translated_tokens[0], skip_special_tokens=True)
+    return translated_text
 
 # Handle translation
 if st.button("TRANSLATE", key="translate_button"):
-    if(input_text == ""):
+    if input_text == "":
         st.error("Please enter text to translate!")
     else:
         translated_text = translate_text(input_text)
         with st.spinner('Wait for it...'):
             time.sleep(2)
-        st.session_state.output_text = translated_text
+        # Label of Icon translation and Vietnamese language
+        st.markdown('<div class="arrow-container"><div class="arrow">⇄</div></div>', unsafe_allow_html=True)
+        st.markdown('<div class="custom-label">VIETNAMESE</div>', unsafe_allow_html=True)
+        # Output text area
+        st.text_area(label = "",value= translated_text, height=150, key="output_text", placeholder="Translated text will appear here...", label_visibility="collapsed")
 
-# Label of Icon translation and Vietnamese language
-st.markdown('<div class="arrow-container"><div class="arrow">⇄</div></div>', unsafe_allow_html=True)
-st.markdown('<div class="custom-label">VIETNAMESE</div>', unsafe_allow_html=True)
 
-# Ensure session state for output text
-if 'output_text' not in st.session_state:
-    st.session_state.output_text = ""
 
-# Output text area
-st.text_area("", height=150, key="output_text", value=st.session_state.output_text, placeholder="Translated text will appear here...", label_visibility="collapsed")
+
